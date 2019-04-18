c7e9d7af6fc156db3c545a36cce62797b713f947
boxxybot <44883226+boxxybot@users.noreply.github.com>
2019-04-18 13:09:11 -0700

    Update config.py

--- config.py
+++ config.py
@@ -1,2 +1,6 @@
-surveyserver_password='password'
-static_path='http://static.mydomain.path'
+db_user='survey_user'
+db_password='survey_user_passsword'
+db_server='survey_database_server'
+db_db='survey_database_name'
+
+static_path='http://static.myethical.healthcare'
