84eae1f5d7ee5a6d7de28c7d63fd9ecd98b35af5
boxxybot <44883226+boxxybot@users.noreply.github.com>
2019-04-18 09:38:08 -0700

    Delete survey.js

--- survey.js
+++
@@ -1,7 +0,0 @@
-var arrivalDate = new Date().getTime();
-var x = setInterval(function() {
-	var now = new Date().getTime();
-	var distance = now - arrivalDate;
-	var seconds = Math.floor(distance / 1000);
-	document.getElementById("blarg").value = seconds;
-}, 1000);
